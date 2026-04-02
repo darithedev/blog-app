09166d950d0a9cfacd38c46b09fe6464874f0e89
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-02 18:37:52 -0400

    Add GET /post/:id to get individual post by id number

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -18,4 +18,32 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(id)) {
+            return res.status(400).json({error: 'Invalid post id.'});
+        }
+
+        const result = await pool.query(
+            `SELECT * FROM posts WHERE id = $1`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'This post does not exist.'
+            });
+        }
+
+        return res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error('GET /posts/:id failed:', error);
+        return res.status(500).json({
+            error: 'Error! Could not get individual post.'
+        });
+    }
+});
+
 export default router;
