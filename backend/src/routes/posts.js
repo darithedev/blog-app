2a765801cef8dc9172dfe3edcb84a44b86b1fd77
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-02 17:30:57 -0400

    Add posts router skeleton

---
+++ backend/src/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+export default router;
