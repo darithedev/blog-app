95917340d2afeb2508240f3fbe49aceab80ff250
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-02 18:21:40 -0400

    Import pools, add GET /posts route to fetch all posts

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import pool from '../db/pools/js'
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
+        const result = await pool.query(
+            `SELECT * from posts ORDER BY created_at DESC`
+        );
 
+        return res.status(200).json(result.rows);
     } catch (error) {
-
+        console.error('GET /posts failed:', error);
+        return res.status(500).json({
+            error: 'Error! Could not get all posts.'
+        });
     }
 });
 
