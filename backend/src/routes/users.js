aab67b45823533c1ed6b071fdacbfbaa416b1f7b
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-03 22:39:57 -0400

    Add POST /users route to create a new user

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -15,4 +15,52 @@ router.get('/', async (req, res) =>{
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const { first_name, last_name, email, password } = req.body;
+
+        if (!first_name || !last_name || !email || ! password) {
+            return res.status(400).json({
+                error: "Valid first name, last name, email, and password is required."
+            });
+        };
+
+        if (first_name.length < 2 || last_name.length < 2) {
+            return res.status(400).json({
+                error: "A valid first and last name with more than 2 characters is required."
+            });
+        };
+            
+        if (!email.includes('@')) {
+            return res.status({ error: "A valid email is required." });
+        };
+
+        if (password.length < 12) {
+            res.status(400).json({
+                error: "Password must be of length of more than 12 characters."
+            });
+        };
+
+        const result = await pool.query(
+            `INSERT INTO users (first_name, last_name, email, password)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *`,
+            [first_name, last_name, email, password]
+        );
+
+        return res.status(200).json(result.rows[0]);
+    } catch (error) {
+        if (error.code === '23505') {
+            return res.status(400).json({
+                error: "This email is associated with an existing account."
+            });
+        };
+
+        console.error('GET /users failed:', error);
+        return res.status(500).json({
+            error: 'Could not create new user.'
+        });
+    }
+});
+
 export default router;
