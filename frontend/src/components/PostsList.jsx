d64776607a740726380c190d079e86a343ee7f62
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-08 23:56:12 -0400

    Add getPost fetch to get list of post from express backend and set result to postList setter

--- frontend/src/components/PostsList.jsx
+++ frontend/src/components/PostsList.jsx
@@ -5,6 +5,21 @@ const API_URL = import.meta.env.VITE_API_URL;
 const PostsList = () => {
     const [postList, setPostList] = useState([]);
 
+    const getPosts = async () => {
+        const url = `${API_URL}/posts`;
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            
+            const result = await response.json();
+            setPostList(result);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <div className="posts-container">
         
