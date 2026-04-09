0994a9764a23895f6bf0ee8a885ad98bf6cb9da8
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-09 00:43:34 -0400

    Import Post componentand add Route path /posts/:id

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import PostsList from './components/PostsList.jsx'
+import Post from './components/Post.jsx'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<PostsList />} />
+      <Route path="/posts/:id" element={<Post />} />
     </Routes>
   )
 }
