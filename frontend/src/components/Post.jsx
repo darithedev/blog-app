97109a150b35bef673af75a55be3538287bffd62
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-09 00:51:52 -0400

    Import useParams from react-router_dom, add useParam id and getPost fetch to retrieve post by post id and set post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,10 +1,30 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const Post = () => {
     const [post, setPost] = useState({});
+    const { id } = useParams();
+
+    const getPost = async () => {
+        const url = `${API_URL}/posts/${id}`;
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            
+            const result = await response.json();
+            setPost(result);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
 
     useEffect(() => {
-
+        getPost();
     }, []);
 
     return (
