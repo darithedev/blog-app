b3321d954941103daaffc3dcc27fef8cb126d858
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-08 23:57:36 -0400

    Add useEffect for getPost fetch

--- frontend/src/components/PostsList.jsx
+++ frontend/src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -19,6 +19,10 @@ const PostsList = () => {
             console.error(error.message);
         }
     }
+
+    useEffect(() => {
+        getPosts();
+    }, []);
 
     return (
         <div className="posts-container">
