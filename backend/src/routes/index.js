3fbcc62411ef0a4d6b3c91a8162291ecca3aac4f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 19:30:26 -0400

    Add textToSpeech router and mount at /text-to-speech

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import usersRouter from './users.js'
 import postsRouter from './posts.js'
+import textToSpeechRouter from './text-to-speech.js'
 
 const router = express.Router();
 
 router.use('/users', usersRouter);
 router.use('/posts', postsRouter);
+router.use('/text-to-speech', textToSpeechRouter);
 
 export default router;
