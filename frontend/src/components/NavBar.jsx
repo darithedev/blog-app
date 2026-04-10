a148a9babfed3bfa59a2be13a7b8130a2cb6982e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 01:04:38 -0400

    Add NavBar component skeleton

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+const NavBar = () => {
+    
+    return (
+        <div className="navbar-container">
+            
+        </div>
+    )
+}
+
+export default NavBar;
