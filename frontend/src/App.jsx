995fcbc686065cf7829178393ec6660bc35b3e82
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 11:41:23 -0400

    Import Me component, add user and loggedIn useState and setters, pass user and loggedin props to NavBar component, and setUser prop and isLoggedIn to Me component, update Post component path to /posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,18 @@ import NavBar from './components/NavBar.jsx'
 import PostsList from './components/PostsList.jsx'
 import Post from './components/Post.jsx'
 import PostForm from './components/PostForm.jsx'
+import Me from './components/Me.jsx'
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loggedIn, isLoggedIn] = useState(false);
 
   return (
     <>
-      <NavBar loggedUser={loggedUser} />
+      <NavBar user={user} loggedIn={loggedIn}/>
       <Routes>
-        <Route path="/" element={<PostsList />} />
+        <Route path="/" element={<Me setUser={setUser} isLoggedIn={isLoggedIn}/>} />
+        <Route path="/posts" element={<PostsList />} />
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/new-post/:id" element={<PostForm />} />
       </Routes>
