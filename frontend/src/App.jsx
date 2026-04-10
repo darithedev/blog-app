5dc5658408cc1cc87c995869407d88ace30e3b23
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 01:20:52 -0400

    Import PostList and PostForm components, render NavBar and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar.jsx'
 import PostsList from './components/PostsList.jsx'
 import Post from './components/Post.jsx'
+import PostForm from './components/PostForm.jsx'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<PostsList />} />
-      <Route path="/posts/:id" element={<Post />} />
-    </Routes>
+    <>
+      <NavBar loggedUser={loggedUser} />
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/posts/:id" element={<Post />} />
+        <Route path="/new-post/:id" element={<PostForm />} />
+      </Routes>
+    </>
   )
 }
 
