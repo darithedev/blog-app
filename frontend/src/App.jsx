3daaa1b1e6550c23a994c97fc92726f2a22e3452
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 13:26:20 -0400

    add postList, filtered useState and setters, Add postList and setFiltered props passed to NavBar component, pass pList: setPostList and filtered props to PostList component to r ender filtered posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,21 @@ import Me from './components/Me.jsx'
 function App() {
   const [user, setUser] = useState(null);
   const [loggedIn, isLoggedIn] = useState(false);
+  const [postList, setPostList] = useState([]);
+  const [filtered, setFiltered] = useState([]);
 
   return (
     <>
-      <NavBar user={user} loggedIn={loggedIn} isLoggedIn={isLoggedIn} />
+      <NavBar 
+        user={user} 
+        postList={postList}
+        setFiltered={setFiltered}
+        loggedIn={loggedIn} 
+        isLoggedIn={isLoggedIn} 
+      />
       <Routes>
         <Route path="/" element={<Me setUser={setUser} isLoggedIn={isLoggedIn}/>} />
-        <Route path="/posts" element={<PostsList />} />
+        <Route path="/posts" element={<PostsList pList={setPostList} filtered={filtered}/>} />
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/new-post/:id" element={<PostForm />} />
       </Routes>
