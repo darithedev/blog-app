d8cc31d9b6df808412ddcf42ddfbe2c68a44be67
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-09 22:48:24 -0400

    Add id useParams, post useState and setter

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -4,6 +4,14 @@ import { Form, Button } from 'react-bootstrap'
 const API_URL = import.meta.env.VITE_API_URL;
 
 const PostForm = () => {
+    const { id } = useParams();
+    const [post, setPost] = useState({
+        user_id: id,
+        title: "",
+        description: "",
+        text: "",
+        tags: []
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
