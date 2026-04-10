7deee752391a2e07b33571b99ee747f0c595fcca
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 11:59:33 -0400

    Import Me.css styling, Add welcome message and instructions, refactor userCard section to select email, add create account link with alert that feat is in progress, add Me.css for styling to Me component

--- frontend/src/components/Me.jsx
+++ frontend/src/components/Me.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import './Me.css'
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -27,14 +28,17 @@ const Me = ({ setUser, isLoggedIn }) => {
 
     return (
         <div className="me-container">
+            <h3>Welcome Back</h3>
+            <p>Select your email below</p>
             {userList.map((user) => (
                 <div className="user-card" key={user.id}>
-                    <span>{user.first_name}</span>
+                    <span>{user.email}</span>
                     <button onClick={() => { setUser(user), isLoggedIn(true), navigate("/posts") }}>
                         Select
                     </button>
                 </div>
             ))}
+            <p id="create-account">Don't see your email? <a href="#" onClick={(() => alert("Feature coming soon."))}>Create An Account</a></p>
         </div>
     )
 }
