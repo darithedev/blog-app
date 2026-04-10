212273e97405d014b9d551ee6a2ad9a035c189e0
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-10 13:34:00 -0400

    Add user, postList, setFiltered, LoggedIn, isLoggedIn props, add search, index, useState and setters, content and fontColor array with respective content and colors, and add navigate const

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
-const NavBar = () => {
-    
+const NavBar = ({ user, postList, setFiltered, loggedIn, isLoggedIn }) => {
+    const [search, setSearch] = useState("");
+    const content = ["Music", "Movies", "Books", "Food"];
+    const fontColor = ["red", "blue", "blueviolet", "orange"];
+    const [index, setIndex] = useState(0);
+    const navigate = useNavigate();
+
     return (
         <div className="navbar-container">
             
