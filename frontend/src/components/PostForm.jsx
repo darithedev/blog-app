020525fc8859f48b5a602f5638a5e38b3aa230f4
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-09 22:43:32 -0400

    Add Form skeleton

---
+++ frontend/src/components/PostForm.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const PostForm = () => {
+
+    return (
+        <Form 
+            className="post-form"
+            onSubmit={handleSubmit}
+        >
+            
+        </Form>
+    )
+
+}
+
+export default PostForm; 
